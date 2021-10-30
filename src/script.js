6d559a3a7bc20633373e39a53029cda7f061ab78
LotteTop <71435799+LotteTop@users.noreply.github.com>
2021-10-30 22:00:42 +0200

    Basically all things are improved

--- src/script.js
+++ src/script.js
@@ -1,20 +1,93 @@
-function citySearch(event) {
-  event.preventDefault();
+function showTemperature(response) {
+  console.log(response);
   let searchInput = document.querySelector("#search-input");
-  let citysearch = document.querySelector("h2");
-  citysearch.innerHTML = searchInput.value;
-  let city = searchInput.value;
+  let citysearch = document.querySelector(".city-name");
+  citysearch.innerHTML = response.data.name;
+  celsiusTemperature = response.data.main.temp;
+  let temperature = Math.round(celciusTemperature);
+  let tempElement = document.querySelector(".temp");
+  tempElement.innerHTML = `${temperature}`;
+  let wind = Math.round(response.data.wind.speed);
+  let windElement = document.querySelector(".wind");
+  windElement.innerHTML = `${wind}`;
+  let humidity = response.data.main.humidity;
+  let humidityElement = document.querySelector(".humidity");
+  humidityElement.innerHTML = `${humidity}`;
+  let description = response.data.weather[0].description;
+  let descriptionElement = document.querySelector(".description");
+  descriptionElement.innerHTML = `${description}`;
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let largeIcon = response.data.weather[0].icon;
+  let largIconElement = document.querySelector("#large-icon");
+  largIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${largeIcon}@2x.png`
+  );
+}
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  console.log(date);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function citySearch(name) {
   let apiKey = "2ff29bed3181c3526c35cc5408037f85";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
 }
 
-let searchform = document.querySelector("form");
-searchform.addEventListener("submit", citySearch);
-
-function showTemperature(response) {
-  console.log(response);
-  let temperature = Math.round(response.data.main.temp);
-  let h1 = document.querySelector("#temperature");
-  h1.innerHTML = `${temperature}°C`;
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#search-input");
+  citySearch(cityInputElement.value);
 }
+
+function showFahrenheit(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = Math.round(celsiusTemperature * (9 / 5) + 32);
+  let tempElement = document.querySelector("#temp");
+  tempElement.innerHTML = fahrenheitTemperature;
+}
+
+function showCelsius(event) {
+  event.preventDefault();
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  let tempElement = document.querySelector("#temp");
+  tempElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let searchform = document.querySelector("#search-form");
+searchform.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", showFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", showCelsius);
+
+citySearch("Assen");
